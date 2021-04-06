f01e3605e01f114dc5d141dafcdd8e21be79dd1f
vicky <vicky.7dev@gmail.com>
2021-04-06 21:05:10 +0530

    Global styles added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import GlobalStyles from "./Components/GlobalStyles";
+
 function App() {
     return (
         <>
+            <GlobalStyles />
             <h1>Hello</h1>
         </>
     );
